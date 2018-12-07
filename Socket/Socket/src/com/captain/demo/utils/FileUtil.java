504f064d7af580bf7536710ad271a7a43235b245
FLDanc <43211850+FLDanc@users.noreply.github.com>
2018-12-07 22:50:07 +0800

    网络通讯

---
+++ Socket/Socket/src/com/captain/demo/utils/FileUtil.java
@@ -0,0 +1,22 @@
+package com.captain.demo.utils;
+
+import java.util.List;
+
+public class FileUtil {
+
+	public static Object getFullPath(String path, String filename) {
+		
+		return null;
+	}
+
+	public static boolean writeFile(Object fullPath, List<String> context, boolean b) {
+		
+		return false;
+	}
+
+	public static List<String> readFile(String path) {
+		
+		return null;
+	}
+
+}
